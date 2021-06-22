19a36de1962edcdb417e8839c87b80e06492b2b4
Nikan Radan <github.com@alias.nikanradan.com>
2021-06-22 03:38:31 -0700

    Fix conflict with Expanded Axe Enchanting

--- Expanded Trident Enchanting/src/main/java/com/smushytaco/expanded_trident_enchanting/mixins/TridentsCanHaveOtherWeaponEnchantmentTypes.java
+++ Expanded Trident Enchanting/src/main/java/com/smushytaco/expanded_trident_enchanting/mixins/TridentsCanHaveOtherWeaponEnchantmentTypes.java
@@ -1,9 +1,6 @@
 package com.smushytaco.expanded_trident_enchanting.mixins;
 import com.smushytaco.expanded_trident_enchanting.ExpandedTridentEnchanting;
 import net.minecraft.enchantment.DamageEnchantment;
-import net.minecraft.enchantment.Enchantment;
-import net.minecraft.enchantment.EnchantmentTarget;
-import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.TridentItem;
 import org.spongepowered.asm.mixin.Mixin;
@@ -11,10 +8,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(DamageEnchantment.class)
-public abstract class TridentsCanHaveOtherWeaponEnchantmentTypes extends Enchantment {
-    protected TridentsCanHaveOtherWeaponEnchantmentTypes(Rarity weight, EnchantmentTarget type, EquipmentSlot[] slotTypes) {
-        super(weight, type, slotTypes);
-    }
+public abstract class TridentsCanHaveOtherWeaponEnchantmentTypes {
     @Inject(method = "isAcceptableItem", at = @At("HEAD"), cancellable = true)
     public void isAcceptableItem(ItemStack stack, CallbackInfoReturnable<Boolean> cir) {
         if (!ExpandedTridentEnchanting.INSTANCE.getConfig().getCanUseOtherWeaponEnchantmentTypesOnTrident() || !(stack.getItem() instanceof TridentItem)) return;
