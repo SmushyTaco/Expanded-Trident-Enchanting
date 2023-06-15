6021233169c7fe3022405c058f14e271739be30d
SmushyTaco <smushytaco@gmail.com>
2023-06-15 14:28:25 -0700

    1.20.1

--- Expanded Trident Enchanting/src/main/java/com/smushytaco/expanded_trident_enchanting/mixins/TridentsCanHaveOtherWeaponEnchantmentTypes.java
+++ Expanded Trident Enchanting/src/main/java/com/smushytaco/expanded_trident_enchanting/mixins/TridentsCanHaveOtherWeaponEnchantmentTypes.java
@@ -1,17 +1,13 @@
 package com.smushytaco.expanded_trident_enchanting.mixins;
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import com.smushytaco.expanded_trident_enchanting.ExpandedTridentEnchanting;
 import net.minecraft.enchantment.DamageEnchantment;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.TridentItem;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(DamageEnchantment.class)
 public abstract class TridentsCanHaveOtherWeaponEnchantmentTypes {
-    @Inject(method = "isAcceptableItem", at = @At("HEAD"), cancellable = true)
-    public void isAcceptableItem(ItemStack stack, CallbackInfoReturnable<Boolean> cir) {
-        if (!ExpandedTridentEnchanting.INSTANCE.getConfig().getCanUseOtherWeaponEnchantmentTypesOnTrident() || !(stack.getItem() instanceof TridentItem)) return;
-        cir.setReturnValue(true);
-    }
+    @ModifyReturnValue(method = "isAcceptableItem", at = @At("RETURN"))
+    public boolean isAcceptableItem(boolean original, ItemStack stack) { return ExpandedTridentEnchanting.INSTANCE.getConfig().getCanUseOtherWeaponEnchantmentTypesOnTrident() && stack.getItem() instanceof TridentItem || original; }
 }
