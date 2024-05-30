32e10c524edb3c6e81f984d1be02ef28be51e58d
Nikan Radan <personal@nikanradan.com>
2024-05-30 15:58:55 -0700

    1.20.6

--- Expanded Trident Enchanting/src/main/java/com/smushytaco/expanded_trident_enchanting/mixins/TridentModification.java
+++ Expanded Trident Enchanting/src/main/java/com/smushytaco/expanded_trident_enchanting/mixins/TridentModification.java
@@ -4,6 +4,7 @@ import com.smushytaco.expanded_trident_enchanting.ExpandedTridentEnchanting;
 import net.minecraft.enchantment.*;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.TridentItem;
+import net.minecraft.registry.tag.ItemTags;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -12,18 +13,21 @@ import org.spongepowered.asm.mixin.injection.At;
 public abstract class TridentModification {
     @Shadow
     @Final
-    public EnchantmentTarget target;
+    private Enchantment.Properties properties;
     @ModifyReturnValue(method = "isAcceptableItem", at = @At("RETURN"))
     public boolean isAcceptableItem(boolean original, ItemStack stack) {
         Enchantment enchantment = (Enchantment) (Object) this;
-        if (enchantment instanceof FireAspectEnchantment) {
+        if (enchantment == Enchantments.FIRE_ASPECT) {
             if (!ExpandedTridentEnchanting.INSTANCE.getConfig().getCanUseFireAspectOnTrident() || !(stack.getItem() instanceof TridentItem)) return original;
             return true;
-        } else if (enchantment instanceof KnockbackEnchantment) {
+        } else if (enchantment == Enchantments.KNOCKBACK) {
             if (!ExpandedTridentEnchanting.INSTANCE.getConfig().getCanUseKnockbackOnTrident() || !(stack.getItem() instanceof TridentItem)) return original;
             return true;
         }  else if (enchantment instanceof LuckEnchantment) {
-            if (!ExpandedTridentEnchanting.INSTANCE.getConfig().getCanUseLootingOnTrident() || target != EnchantmentTarget.WEAPON || !(stack.getItem() instanceof TridentItem)) return original;
+            if (!ExpandedTridentEnchanting.INSTANCE.getConfig().getCanUseLootingOnTrident() || properties.supportedItems() != ItemTags.SWORD_ENCHANTABLE || !(stack.getItem() instanceof TridentItem)) return original;
+            return true;
+        } else if (enchantment instanceof DamageEnchantment) {
+            if (!ExpandedTridentEnchanting.INSTANCE.getConfig().getCanUseOtherWeaponEnchantmentTypesOnTrident() || !(stack.getItem() instanceof TridentItem)) return original;
             return true;
         }
         return original;
--- Expanded Trident Enchanting/src/main/kotlin/com/smushytaco/expanded_trident_enchanting/ExpandedTridentEnchantingPreLaunch.kt
+++
@@ -1,5 +0,0 @@
-package com.smushytaco.expanded_trident_enchanting
-import com.llamalad7.mixinextras.MixinExtrasBootstrap
-import net.fabricmc.loader.api.entrypoint.PreLaunchEntrypoint
-@Suppress("UNUSED")
-object ExpandedTridentEnchantingPreLaunch: PreLaunchEntrypoint { override fun onPreLaunch() { MixinExtrasBootstrap.init() } }
