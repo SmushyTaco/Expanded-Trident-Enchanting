796f06422ed2b75654d20ca1f709a749bf1ce41a
Nikan Radan <github.com@alias.nikanradan.com>
2024-06-14 14:50:17 -0700

    1.21

--- Expanded Trident Enchanting/src/main/java/com/smushytaco/expanded_trident_enchanting/mixins/TridentModification.java
+++ Expanded Trident Enchanting/src/main/java/com/smushytaco/expanded_trident_enchanting/mixins/TridentModification.java
@@ -4,29 +4,23 @@ import com.smushytaco.expanded_trident_enchanting.ExpandedTridentEnchanting;
 import net.minecraft.enchantment.*;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.TridentItem;
-import net.minecraft.registry.tag.ItemTags;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 @Mixin(Enchantment.class)
 public abstract class TridentModification {
-    @Shadow
-    @Final
-    private Enchantment.Properties properties;
     @ModifyReturnValue(method = "isAcceptableItem", at = @At("RETURN"))
     public boolean isAcceptableItem(boolean original, ItemStack stack) {
         Enchantment enchantment = (Enchantment) (Object) this;
-        if (enchantment == Enchantments.FIRE_ASPECT) {
+        if (ExpandedTridentEnchanting.INSTANCE.isSameEnchantment(enchantment, Enchantments.FIRE_ASPECT)) {
             if (!ExpandedTridentEnchanting.INSTANCE.getConfig().getCanUseFireAspectOnTrident() || !(stack.getItem() instanceof TridentItem)) return original;
             return true;
-        } else if (enchantment == Enchantments.KNOCKBACK) {
+        } else if (ExpandedTridentEnchanting.INSTANCE.isSameEnchantment(enchantment, Enchantments.KNOCKBACK)) {
             if (!ExpandedTridentEnchanting.INSTANCE.getConfig().getCanUseKnockbackOnTrident() || !(stack.getItem() instanceof TridentItem)) return original;
             return true;
-        }  else if (enchantment instanceof LuckEnchantment) {
-            if (!ExpandedTridentEnchanting.INSTANCE.getConfig().getCanUseLootingOnTrident() || properties.supportedItems() != ItemTags.SWORD_ENCHANTABLE || !(stack.getItem() instanceof TridentItem)) return original;
+        }  else if (ExpandedTridentEnchanting.INSTANCE.isSameEnchantment(enchantment, Enchantments.LOOTING)) {
+            if (!ExpandedTridentEnchanting.INSTANCE.getConfig().getCanUseLootingOnTrident() || !(stack.getItem() instanceof TridentItem)) return original;
             return true;
-        } else if (enchantment instanceof DamageEnchantment) {
+        } else if (ExpandedTridentEnchanting.INSTANCE.isSameEnchantment(enchantment, Enchantments.SHARPNESS) || ExpandedTridentEnchanting.INSTANCE.isSameEnchantment(enchantment, Enchantments.SMITE) || ExpandedTridentEnchanting.INSTANCE.isSameEnchantment(enchantment, Enchantments.BANE_OF_ARTHROPODS)) {
             if (!ExpandedTridentEnchanting.INSTANCE.getConfig().getCanUseOtherWeaponEnchantmentTypesOnTrident() || !(stack.getItem() instanceof TridentItem)) return original;
             return true;
         }
