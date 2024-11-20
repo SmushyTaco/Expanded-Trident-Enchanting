60041ec8990395b73985f6dfbe01eb98c19ba40b
Nikan Radan <github.com@alias.nikanradan.com>
2024-11-20 15:42:19 -0800

    Mace Enchantment Support

--- Expanded Trident Enchanting/src/main/java/com/smushytaco/expanded_trident_enchanting/mixins/TridentModification.java
+++ Expanded Trident Enchanting/src/main/java/com/smushytaco/expanded_trident_enchanting/mixins/TridentModification.java
@@ -4,8 +4,10 @@ import com.smushytaco.expanded_trident_enchanting.ExpandedTridentEnchanting;
 import net.minecraft.enchantment.*;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.TridentItem;
+import net.minecraft.registry.entry.RegistryEntry;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import java.util.Set;
 @Mixin(Enchantment.class)
 public abstract class TridentModification {
     @ModifyReturnValue(method = "isAcceptableItem", at = @At("RETURN"))
@@ -17,8 +19,17 @@ public abstract class TridentModification {
         } else if (ExpandedTridentEnchanting.INSTANCE.isSameEnchantment(enchantment, Enchantments.KNOCKBACK)) {
             if (!ExpandedTridentEnchanting.INSTANCE.getConfig().getCanUseKnockbackOnTrident() || !(stack.getItem() instanceof TridentItem)) return original;
             return true;
-        }  else if (ExpandedTridentEnchanting.INSTANCE.isSameEnchantment(enchantment, Enchantments.LOOTING)) {
+        } else if (ExpandedTridentEnchanting.INSTANCE.isSameEnchantment(enchantment, Enchantments.LOOTING)) {
             if (!ExpandedTridentEnchanting.INSTANCE.getConfig().getCanUseLootingOnTrident() || !(stack.getItem() instanceof TridentItem)) return original;
+            return true;
+        } else if (ExpandedTridentEnchanting.INSTANCE.isSameEnchantment(enchantment, Enchantments.DENSITY)) {
+            if (!ExpandedTridentEnchanting.INSTANCE.getConfig().getCanUseDensityOnTrident() || !(stack.getItem() instanceof TridentItem)) return original;
+            return true;
+        } else if (ExpandedTridentEnchanting.INSTANCE.isSameEnchantment(enchantment, Enchantments.BREACH)) {
+            if (!ExpandedTridentEnchanting.INSTANCE.getConfig().getCanUseBreachOnTrident() || !(stack.getItem() instanceof TridentItem)) return original;
+            return true;
+        } else if (ExpandedTridentEnchanting.INSTANCE.isSameEnchantment(enchantment, Enchantments.WIND_BURST)) {
+            if (!ExpandedTridentEnchanting.INSTANCE.getConfig().getCanUseWindBurstOnTrident() || !(stack.getItem() instanceof TridentItem)) return original;
             return true;
         } else if (ExpandedTridentEnchanting.INSTANCE.isSameEnchantment(enchantment, Enchantments.SHARPNESS) || ExpandedTridentEnchanting.INSTANCE.isSameEnchantment(enchantment, Enchantments.SMITE) || ExpandedTridentEnchanting.INSTANCE.isSameEnchantment(enchantment, Enchantments.BANE_OF_ARTHROPODS)) {
             if (!ExpandedTridentEnchanting.INSTANCE.getConfig().getCanUseOtherWeaponEnchantmentTypesOnTrident() || !(stack.getItem() instanceof TridentItem)) return original;
@@ -26,4 +37,6 @@ public abstract class TridentModification {
         }
         return original;
     }
+    @ModifyReturnValue(method = "canBeCombined", at = @At("RETURN"))
+    private static boolean hookCanBeCombined(boolean original, RegistryEntry<Enchantment> first, RegistryEntry<Enchantment> second) { return original || ExpandedTridentEnchanting.INSTANCE.getConfig().getImpalingAndOtherWeaponEnchantmentTypesCanBeMixed() && (ExpandedTridentEnchanting.INSTANCE.canCombineEnchantments(first, second, Set.of(Enchantments.IMPALING, Enchantments.SMITE)) || ExpandedTridentEnchanting.INSTANCE.canCombineEnchantments(first, second, Set.of(Enchantments.IMPALING, Enchantments.BANE_OF_ARTHROPODS)) || ExpandedTridentEnchanting.INSTANCE.canCombineEnchantments(first, second, Set.of(Enchantments.IMPALING, Enchantments.SHARPNESS))); }
 }
--- Expanded Trident Enchanting/src/main/kotlin/com/smushytaco/expanded_trident_enchanting/ExpandedTridentEnchanting.kt
+++ Expanded Trident Enchanting/src/main/kotlin/com/smushytaco/expanded_trident_enchanting/ExpandedTridentEnchanting.kt
@@ -9,6 +9,7 @@ import net.minecraft.enchantment.Enchantment
 import net.minecraft.registry.DynamicRegistryManager
 import net.minecraft.registry.RegistryKey
 import net.minecraft.registry.RegistryKeys
+import net.minecraft.registry.entry.RegistryEntry
 import kotlin.jvm.optionals.getOrNull
 object ExpandedTridentEnchanting : ModInitializer {
     const val MOD_ID = "expanded_trident_enchanting"
@@ -23,4 +24,5 @@ object ExpandedTridentEnchanting : ModInitializer {
         ServerLifecycleEvents.SERVER_STARTED.register { server -> dynamicRegistryManager = server.registryManager }
     }
     fun isSameEnchantment(enchantment: Enchantment, enchantmentRegistryKey: RegistryKey<Enchantment>) = dynamicRegistryManager?.getOptional(RegistryKeys.ENCHANTMENT)?.getOrNull()?.getEntry(enchantment)?.matchesKey(enchantmentRegistryKey) ?: false
+    fun canCombineEnchantments(first: RegistryEntry<Enchantment>, second: RegistryEntry<Enchantment>, enchantments: Set<RegistryKey<Enchantment>>) = enchantments.any { first.matchesKey(it) } && enchantments.any { second.matchesKey(it) } && !first.matchesKey(second.key.getOrNull())
 }
