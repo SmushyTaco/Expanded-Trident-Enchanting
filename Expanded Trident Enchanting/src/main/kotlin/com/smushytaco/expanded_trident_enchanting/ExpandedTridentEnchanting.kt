b6e42a2240880f9bcf3d323bbe2bfdb75c282ee8
Nikan Radan <github.com@alias.nikanradan.com>
2024-11-25 16:07:28 -0800

    New Config

--- Expanded Trident Enchanting/src/main/kotlin/com/smushytaco/expanded_trident_enchanting/ExpandedTridentEnchanting.kt
+++ Expanded Trident Enchanting/src/main/kotlin/com/smushytaco/expanded_trident_enchanting/ExpandedTridentEnchanting.kt
@@ -1,8 +1,4 @@
 package com.smushytaco.expanded_trident_enchanting
-import com.smushytaco.expanded_trident_enchanting.configuration_support.ModConfiguration
-import me.shedaniel.autoconfig.AutoConfig
-import me.shedaniel.autoconfig.annotation.Config
-import me.shedaniel.autoconfig.serializer.GsonConfigSerializer
 import net.fabricmc.api.ModInitializer
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents
 import net.minecraft.enchantment.Enchantment
@@ -13,14 +9,9 @@ import net.minecraft.registry.entry.RegistryEntry
 import kotlin.jvm.optionals.getOrNull
 object ExpandedTridentEnchanting : ModInitializer {
     const val MOD_ID = "expanded_trident_enchanting"
-    lateinit var config: ModConfiguration
-        private set
+    val config = ModConfig.createAndLoad()
     private var dynamicRegistryManager: DynamicRegistryManager? = null
     override fun onInitialize() {
-        AutoConfig.register(ModConfiguration::class.java) { definition: Config, configClass: Class<ModConfiguration> ->
-            GsonConfigSerializer(definition, configClass)
-        }
-        config = AutoConfig.getConfigHolder(ModConfiguration::class.java).config
         ServerLifecycleEvents.SERVER_STARTED.register { server -> dynamicRegistryManager = server.registryManager }
     }
     fun isSameEnchantment(enchantment: Enchantment, enchantmentRegistryKey: RegistryKey<Enchantment>) = dynamicRegistryManager?.getOptional(RegistryKeys.ENCHANTMENT)?.getOrNull()?.getEntry(enchantment)?.matchesKey(enchantmentRegistryKey) ?: false
